f9a9690e67482fbec3d1dea752c4aece8c68cefc
Jefferson Alquinga <55715332+Jeffers1998@users.noreply.github.com>
2019-11-18 13:53:16 -0500

    Resubido

---
+++ 02- Iterables/01-while.ts
@@ -0,0 +1,44 @@
+// DO WHILE
+let vecesQueHeComido = 0;
+do {
+    console.log(´Vamos a comer´);
+    veesQueHeComido = vecesQueComido +1;
+    switch(vecesQueHeComido){
+        case 1:
+            console.log(´Desayuno´);
+            break;
+            case 2 ;
+              console.log(´Almuerzo´);
+              break;
+              case 3: 
+              console.log(´Meriendo´);
+              break;
+              default:
+                  console.log(´Gorditos´);
+
+
+
+    }
+
+} While(vecesQueHeComido <3 )
+/*
+1 Analisis socioecnomico
+2 Algebra
+3 compiladores 
+5 Fundamentos de programacion 
+7 Calculo
+11 Fisica
+13 Ingles
+
+
+si es impar -> aumentamos a analisis socio economico 
+si es par - > aumentamos a algebra 
+si es multiplo de 3 -> aumentamos a compiladores 
+*/ 
+
+let analisis= 0;
+console.log (´Hice ${Analisis}deberes de Analisis.´);
+            // templace strings ->tilde invertida= alt + 96 
+if(par){
+
+}else if 
