f2a3015c2fb922ef2b04a57a248119c632c46cbb
Jefferson Alquinga <55715332+Jeffers1998@users.noreply.github.com>
2019-11-25 14:19:46 -0500

    Funciones

---
+++ 03-funciones/01-introduccion.ts
@@ -0,0 +1,24 @@
+function log(  // Parametro
+    ){
+       console.log();
+
+}
+log("MALDICION");
+
+
+
+function noHaYElTexto(){
+   console.log("No hay el texto")
+
+}
+noHaYElTexto();
+
+
+
+function sumarDosNumeros(
+ a: number,
+ b: number
+ ):number{
+ return  a + b ;
+}
+sumarDosNumeros( 2, 2);
